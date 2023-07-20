97750ed83188e04f614abaa520b5df5e6ecf32b5
Geovanna Otoni <geovannaotoni@gmail.com>
2023-07-20 14:47:59 -0300

    feat: rota 2 e adiciona try/catch da rota 1

--- src/routes/talkerRoutes.js
+++ src/routes/talkerRoutes.js
@@ -4,8 +4,27 @@ const { readData } = require('../utils/fsUtils');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const data = await readData();
-  res.status(200).json(data);
+  try {
+    const data = await readData();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await readData();
+    const findElem = data.find((talker) => Number(id) === talker.id);
+    if (!findElem) {
+      return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+    }
+
+    res.status(200).json(findElem);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
