e724a0fe164beb2d36c9b93269e9b99762d69f46
Geovanna Otoni <geovannaotoni@gmail.com>
2023-07-20 14:26:16 -0300

    [Geovanna Otoni] - Talker Manager

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
-// não remova esse endpoint, e para o avaliador funcionar
+// não remova esse endpoint, e para o avaliador funcionar 
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
